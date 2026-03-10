07b5d31e75cc44f6b32bcf2182500d647ce85adb
Lebedz Misha <lebedz.misha.bsuir@gmail.com>
2026-03-10 13:08:06 +0300

    p

--- src/main/java/com/pricetracker/service/ProductService.java
+++ src/main/java/com/pricetracker/service/ProductService.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public final class ProductService {
+public class ProductService {
 
 
   private final ProductRepository productRepository;
