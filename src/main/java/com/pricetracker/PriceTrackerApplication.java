fe345e26a37b25d7ee0d90e4d840a58e70efc157
Mixeld <87525307+Mixeld@users.noreply.github.com>
2026-03-14 16:38:01 +0300

    Update PriceTrackerApplication.java

--- src/main/java/com/pricetracker/PriceTrackerApplication.java
+++ src/main/java/com/pricetracker/PriceTrackerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PriceTrackerApplication {
 
   public PriceTrackerApplication() {
-
+    /*
+    */
   }
 
   public static void main(final String[] args) {
